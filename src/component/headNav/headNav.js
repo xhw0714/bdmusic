e72ee0fbf93f3d73817681d13d79a7bb08e8be9e
mpx <18364076997@163.com>
2017-12-12 01:22:29 +0800

    主页结构和样式

--- src/component/headNav/headNav.js
+++ src/component/headNav/headNav.js
@@ -1,19 +1,25 @@
 import React,{ Component } from 'react';
-import {Link } from 'react-router-dom'
+import {Link } from 'react-router-dom';
+
+import './style/index.css';
 
 export default class extends Component{
 
     render(){
         return (
-            <nav>
-                <ul>
-                    <li><Link to="/">首页</Link></li>
-                    <li><Link to="/songlists">分类</Link></li>
-                    <li><Link to="/artists">歌手</Link></li>
-                    <li><Link to="/listcate">榜单</Link></li>
-                    <li><Link to="/ucenter">我的</Link></li>
-                    <li><Link to="/search">搜索</Link></li>
-                </ul>
+            <nav className="head clearfix">
+                <div className="logo fl">
+                    <Link to="/">首页</Link>
+                </div>
+                <div className="head-right fr">
+                    <ul className="clearfix">
+                        <li><Link to="/songlists">分类</Link></li>
+                        <li><Link to="/artists">歌手</Link></li>
+                        <li><Link to="/listcate">榜单</Link></li>
+                        <li><Link to="/ucenter">我的</Link></li>
+                        <li className="search-logo"><Link to="/search">搜索</Link></li>
+                    </ul>
+                </div>
             </nav>
         )
     }
--- src/container/index/index.js
+++ src/container/index/index.js
@@ -1,9 +1,62 @@
 import React,{Component} from "react";
 
+import './style/home.css';
+
 export default class arrtists extends Component{
     render(){
         return (
-            <div>index</div>
+            <section className="home-content">
+                <div className="home-today">
+                    <h2 className="home-today-title">今日推荐</h2>
+                    <ul className="home-today-list clearfix">
+                        <li className="home-today-item">
+                            <div className="home-today-item-pic">
+                                <img src={require('./img/1.jpg')} alt=""/>
+                                <span className="home-today-item-play"></span>
+                            </div>
+                            <div className="home-today-item-songname">小酒窝</div>
+                        </li>
+                        <li className="home-today-item">
+                            <div className="home-today-item-pic">
+                                <img src={require('./img/1.jpg')} alt=""/>
+                                <span className="home-today-item-play"></span>
+                            </div>
+                            <div className="home-today-item-songname">小酒窝</div>
+                        </li>
+                        <li className="home-today-item">
+                            <div className="home-today-item-pic">
+                                <img src={require('./img/1.jpg')} alt=""/>
+                                <span className="home-today-item-play"></span>
+                            </div>
+                            <div className="home-today-item-songname">小酒窝</div>
+                        </li>
+                        <li className="home-today-item">
+                            <div className="home-today-item-pic">
+                                <img src={require('./img/1.jpg')} alt=""/>
+                                <span className="home-today-item-play"></span>
+                            </div>
+                            <div className="home-today-item-songname">小酒窝</div>
+                        </li>
+                        <li className="home-today-item">
+                            <div className="home-today-item-pic">
+                                <img src={require('./img/1.jpg')} alt=""/>
+                                <span className="home-today-item-play"></span>
+                            </div>
+                            <div className="home-today-item-songname">小酒窝</div>
+                        </li>
+                        <li className="home-today-item">
+                            <div className="home-today-item-pic">
+                                <img src={require('./img/1.jpg')} alt=""/>
+                                <span className="home-today-item-play"></span>
+                            </div>
+                            <div className="home-today-item-songname">小酒窝</div>
+                        </li>
+                    </ul>
+                </div>
+                <div className="home-new">
+
+                </div>
+            </section>
         )
     }
 }
