0e1ae171329a8d1580177ffd2f2f3dd37516caed
mpx <18364076997@163.com>
2017-12-12 23:19:44 +0800

    新增榜单页

--- src/container/listcate/listcate.js
+++ src/container/listcate/listcate.js
@@ -1,9 +1,51 @@
 import React,{Component} from "react";
+import './style/index.css';
 
 export default class listcate extends Component{
     render(){
         return (
-            <div>listcate</div>
+            <div className="top-page">
+                <div className="item clearfix">
+                    <div className="pic fl">
+                        <img src={require('./img/test.jpg')} alt=""/>
+                        <span></span>
+                    </div>
+                    <ul className="song-box fl">
+                        <li className="song-item">
+                            <span className="num">1</span>
+                            <span className="song-name">背过手 - 薛之谦</span>
+                        </li>
+                        <li className="song-item">
+                            <span className="num">2</span>
+                            <span className="song-name">背过手 - 薛之谦</span>
+                        </li>
+                        <li className="song-item">
+                            <span className="num">3</span>
+                            <span className="song-name">背过手 - 薛之谦</span>
+                        </li>
+                    </ul>
+                </div>
+                <div className="item clearfix">
+                    <div className="pic fl">
+                        <img src={require('./img/test.jpg')} alt=""/>
+                        <span></span>
+                    </div>
+                    <ul className="song-box fl">
+                        <li className="song-item">
+                            <span className="num">1</span>
+                            <span className="song-name">背过手 - 薛之谦</span>
+                        </li>
+                        <li className="song-item">
+                            <span className="num">2</span>
+                            <span className="song-name">背过手 - 薛之谦</span>
+                        </li>
+                        <li className="song-item">
+                            <span className="num">3</span>
+                            <span className="song-name">背过手 - 薛之谦</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         )
     }
 }
