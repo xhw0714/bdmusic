23e7cc93913f9647784beffb403e20ce9d961655
mpx <18364076997@163.com>
2017-12-13 00:25:28 +0800

    新增搜索页

--- src/component/search/search.js
+++ src/component/search/search.js
@@ -1,9 +1,35 @@
 import React,{Component} from "react";
+import './style/index.css';
 
 export default class search extends Component{
     render(){
         return (
-            <div>search</div>
+            <div className="search">
+                <div className="search-input">
+                    <input type="text" placeholder="歌名、歌手等"/>
+                    <span className="cancel">取消</span>
+                </div>
+
+                <ul className="search-text clearfix">
+                    <li className="s-item">红红火火</li>
+                    <li className="s-item">红红火火恍恍惚惚</li>
+                    <li className="s-item">红红火火</li>
+                    <li className="s-item">红红火火恍恍惚惚</li>
+                    <li className="s-item">红红火火</li>
+                    <li className="s-item">红红火火恍恍惚惚</li>
+                    <li className="s-item">红红火火</li>
+                    <li className="s-item">红红火火恍恍惚惚</li>
+                </ul>
+
+                <ul className="search-history">
+                    <li className="h-item">
+                        红红火火恍恍惚惚
+                    </li>
+                </ul>
+                <div className="clear-btn">
+                    清除历史记录
+                </div>
+            </div>
         )
     }
 }
