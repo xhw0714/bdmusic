48bb304b7f93006035a9c517724ac2ff971ddb3d
mpx <18364076997@163.com>
2017-12-13 14:41:31 +0800

    新增搜索结果页

--- src/component/search/search.js
+++ src/component/search/search.js
@@ -5,29 +5,49 @@ export default class search extends Component{
     render(){
         return (
             <div className="search">
-                <div className="search-input">
-                    <input type="text" placeholder="歌名、歌手等"/>
-                    <span className="cancel">取消</span>
-                </div>
+                <div className="hidd">
+                    <div className="search-input">
+                        <input type="text" placeholder="歌名、歌手等"/>
+                        <span className="cancel">取消</span>
+                    </div>
 
-                <ul className="search-text clearfix">
-                    <li className="s-item">红红火火</li>
-                    <li className="s-item">红红火火恍恍惚惚</li>
-                    <li className="s-item">红红火火</li>
-                    <li className="s-item">红红火火恍恍惚惚</li>
-                    <li className="s-item">红红火火</li>
-                    <li className="s-item">红红火火恍恍惚惚</li>
-                    <li className="s-item">红红火火</li>
-                    <li className="s-item">红红火火恍恍惚惚</li>
-                </ul>
+                    <div className="search-content">
+                        <ul className="search-text clearfix">
+                            <li className="s-item">红红火火</li>
+                            <li className="s-item">红红火火恍恍惚惚</li>
+                            <li className="s-item">红红火火</li>
+                            <li className="s-item">红红火火恍恍惚惚</li>
+                            <li className="s-item">红红火火</li>
+                            <li className="s-item">红红火火恍恍惚惚</li>
+                            <li className="s-item">红红火火</li>
+                            <li className="s-item">红红火火恍恍惚惚</li>
+                        </ul>
 
-                <ul className="search-history">
-                    <li className="h-item">
-                        红红火火恍恍惚惚
-                    </li>
-                </ul>
-                <div className="clear-btn">
-                    清除历史记录
+                        <ul className="search-history">
+                            <li className="h-item">
+                                红红火火恍恍惚惚
+                            </li>
+                        </ul>
+                        <div className="clear-btn">
+                            清除历史记录
+                        </div>
+                    </div>
+                    <ul className="searched-list">
+                        <li>
+                            <div className="fl">
+                                <span className="song-name">背过手</span>
+                                <span className="singer">薛之谦</span>
+                            </div>
+                            <div className=" fr"></div>
+                        </li>
+                        <li>
+                            <div className="fl">
+                                <span className="song-name">背过手</span>
+                                <span className="singer">薛之谦</span>
+                            </div>
+                            <div className="down fr"></div>
+                        </li>
+                    </ul>
                 </div>
             </div>
         )
