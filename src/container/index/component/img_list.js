f90b6ce63fcbd0dce06b0cccc48b59580627deaf
xhw0714 <842644519@qq.com>
2017-12-14 11:47:53 +0800

    分类

--- src/container/index/component/img_list.js
+++ src/container/index/component/img_list.js
@@ -4,10 +4,10 @@ export default class ImgList extends React.Component {
 
     render() {
         if(this.props.mes){
-            var {pic_big,title} = this.props.mes;
+            var {pic_big,title,song_id} = this.props.mes;
         }
         return (
-            <li className="home-today-item">
+            <li className="home-today-item" songid={song_id}>
                 <div className="home-today-item-pic">
                     <img src={pic_big} alt=""/>
                     <span className="home-today-item-play"></span>
